0526273dcaf044570856e86932378912c66a90c2
zohidjon <zohidjonakbarov@gmail.com>
2018-03-29 10:41:55 +0500

    some changes on news

--- app/src/main/java/com/example/hp_pk/dictionary/ui/activities/fragments/NewsFragment.java
+++ app/src/main/java/com/example/hp_pk/dictionary/ui/activities/fragments/NewsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,6 +50,7 @@ public class NewsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapterWithProgress(adapter);
         List<News> newsList = new ArrayList<>();
         newsList.add(new News());
